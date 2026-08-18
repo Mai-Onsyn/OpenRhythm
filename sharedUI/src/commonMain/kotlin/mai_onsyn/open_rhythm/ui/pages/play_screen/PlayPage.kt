e6b3c60842436fb7dc48635e6a8ba11c98acbbc1
Mai_Onsyn <254454815@qq.com>
2026-08-18 20:30:37 +0800

    optimize status bar behavior in midi play page

--- sharedUI/src/commonMain/kotlin/mai_onsyn/open_rhythm/ui/pages/play_screen/PlayPage.kt
+++ sharedUI/src/commonMain/kotlin/mai_onsyn/open_rhythm/ui/pages/play_screen/PlayPage.kt
@@ -1,33 +1,52 @@
 package mai_onsyn.open_rhythm.ui.pages.play_screen
 
+import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.animation.core.LinearOutSlowInEasing
 import androidx.compose.animation.core.animateDpAsState
 import androidx.compose.animation.core.tween
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.animation.slideInVertically
+import androidx.compose.animation.slideOutVertically
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.IconButtonDefaults
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.backhandler.BackHandler
+import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.rotate
 import androidx.compose.ui.focus.FocusRequester
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.input.pointer.PointerEventPass
 import androidx.compose.ui.input.pointer.PointerEventType
+import androidx.compose.ui.input.pointer.PointerIcon
+import androidx.compose.ui.input.pointer.pointerHoverIcon
 import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.layout.onSizeChanged
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.unit.dp
 import co.touchlab.kermit.Logger
+import kotlinx.coroutines.delay
 import mai_onsyn.open_rhythm.bridge.Global
 import mai_onsyn.open_rhythm.core.midi.Midi
 import mai_onsyn.open_rhythm.core.midi.bpmAtTick
+import mai_onsyn.open_rhythm.ui.icons.ic_arrow_warm_up
 import mai_onsyn.open_rhythm.ui.modules.midi_flow.MidiDownRegion
 import kotlin.math.roundToInt
 import kotlin.random.Random
+import kotlin.time.Duration.Companion.seconds
 
 @OptIn(ExperimentalComposeUiApi::class)
 @Composable
@@ -46,22 +65,26 @@ fun PlayPage(
     val focusRequester = remember { FocusRequester() }
 
     var statusBarVisible by remember { mutableStateOf(true) }
+    var dropdownButtonVisible by remember { mutableStateOf(false) }
+    var eventInteractState by remember { mutableStateOf(0L)}
     Box(
         modifier = Modifier
             .clip(RectangleShape)
-            .pointerInput(Unit) {
-                awaitPointerEventScope {
-                    while (true) {
-                        val event = awaitPointerEvent(PointerEventPass.Initial)
-                        if (event.type == PointerEventType.Press)
-                            statusBarVisible = true
-                    }
-                }
-            }
     ) {
         MidiDownRegion(
             modifier = Modifier
-                .fillMaxSize(),
+                .fillMaxSize()
+                .pointerInput(Unit) {
+                    awaitPointerEventScope {
+                        while (true) {
+                            val event = awaitPointerEvent(PointerEventPass.Initial)
+                            if (event.type == PointerEventType.Press) {
+                                dropdownButtonVisible = true
+                                eventInteractState++
+                            }
+                        }
+                    }
+                },
             midi = displayMidi,
             trackColors = trackColors,
             isPlaying = isPlaying,
@@ -73,6 +96,40 @@ fun PlayPage(
         )
 
         if (drawStatusBar) {
+            AnimatedVisibility(
+                visible = dropdownButtonVisible,
+                modifier = Modifier
+                    .align(Alignment.TopEnd)
+                    .padding(top = 16.dp, end = 16.dp),
+                enter = slideInVertically() + fadeIn(),
+                exit = slideOutVertically() + fadeOut()
+            ) {
+                IconButton(
+                    onClick = {
+                        statusBarVisible = true
+                        dropdownButtonVisible = false
+                    },
+                    modifier = Modifier.alpha(0.6f).pointerHoverIcon(PointerIcon.Hand),
+                    colors = IconButtonDefaults.iconButtonColors(
+                        containerColor = MaterialTheme.colorScheme.secondaryContainer,
+                        contentColor = MaterialTheme.colorScheme.onSecondaryContainer
+                    )
+                ) {
+                    Icon(
+                        imageVector = ic_arrow_warm_up,
+                        contentDescription = "show status bar",
+                        modifier = Modifier.rotate(180f)
+                    )
+                }
+            }
+
+            LaunchedEffect(dropdownButtonVisible, eventInteractState) {
+                if (dropdownButtonVisible) {
+                    delay(2.seconds)
+                    dropdownButtonVisible = false
+                }
+            }
+
             var statusBarBoxHeight by remember { mutableStateOf(0) }
             val statusBarHeight by animateDpAsState(
                 targetValue = if (statusBarVisible) 0.dp else with(LocalDensity.current) { -statusBarBoxHeight.toDp() },
@@ -82,14 +139,15 @@ fun PlayPage(
             StatusBar(
                 modifier = Modifier
                     .fillMaxWidth()
-                    .onSizeChanged {
-                        statusBarBoxHeight = it.height
-                    }
+                    .onSizeChanged { statusBarBoxHeight = it.height }
                     .offset(y = statusBarHeight),
                 isPlaying = isPlaying,
                 onBack = onBack,
                 onToggledPlay = { isPlaying = it },
-                onHide = { statusBarVisible = false },
+                onHide = {
+                    statusBarVisible = false
+                    dropdownButtonVisible = true
+                },
                 progress = playProgress,
                 onProgressChangeStart = {
                     if (isPlaying) {
